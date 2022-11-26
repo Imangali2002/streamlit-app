c4e65b520c51410d7728010f40cd6a826fa5e885
Imangali <65546538+Imangali2002@users.noreply.github.com>
2022-11-26 15:42:53 +0600

    Delete server.py

--- server.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI, File, UploadFile
-
-app = FastAPI()
-
-@app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile = File(...)):
-    print(file)
-    return {'status': 'ok'}
